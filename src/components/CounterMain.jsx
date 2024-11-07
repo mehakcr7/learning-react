7ea1e036ed3d836bc12524ef4cdd46f8c86765c3
Mehak Vazhayil <mehakvazhayil2002@gmail.com>
2024-11-07 10:15:54 +0530

    created a like heart shaped button and when like button is clicked increase the like count and the like button should turn red. when clicked again turn the button to normal colour and decrement the like count like in Instagram.

--- src/components/CounterMain.jsx
+++ src/components/CounterMain.jsx
@@ -1,20 +1,47 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 function CounterMain() {
-    const [a,setA] =useState(0)
+  const [like, setLike] = useState(false);
+  const [a, setA] = useState(10);
+
+  // Determine the className based on the like state
+  let svgClassName = "w-5 h-5 size-6";
+  if (like) {
+    svgClassName += " bg-red-600";
+  }
+
   return (
     <div>
-      <button onClick={()=>{
-        setA(a+1)
-      }}>click me</button>
+      <div>
+        <svg
+          className={svgClassName}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          onClick={() => {
+            if (like) {
+              setLike(false);
+              setA(a - 1);
+            } else {
+              setLike(true);
+              setA(a + 1);
+            }
+          }}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+          />
+        </svg>
+      </div>
       <br />
-      <button onClick={()=>{
-        setA(a-1)
-      }}>click me</button>
-      <br />
+
       {a}
     </div>
-  )
+  );
 }
 
-export default CounterMain
+export default CounterMain;
